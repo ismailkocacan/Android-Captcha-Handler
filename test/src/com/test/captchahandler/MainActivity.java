e0c5cf73da2a9fe778f599e7c0f665f944e863bd
ismail <ismailkocacan@gmail.com>
2014-07-19 23:51:35 +0300

    JsObject tests

--- test/src/com/test/captchahandler/MainActivity.java
+++ test/src/com/test/captchahandler/MainActivity.java
@@ -2,23 +2,58 @@ package com.test.captchahandler;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-import android.widget.Toast;
+import android.widget.ImageView;
 
-public class MainActivity extends Activity {
+import com.stackdeveloper.lib.HandlerImageDataResult;
+import com.stackdeveloper.lib.ImageUtil;
+import com.stackdeveloper.lib.JsObject;
 
+public class MainActivity extends Activity 
+{
+
+	private final static String URL_FORM="http://runnable.com/U8rWe5yFx-sLegW6/output";
+	
+	private JsObject jsObject;
+	private MyHandlerImageDataResult mMyHandlerImageDataResult;
+	private ImageView mImgCaptcha;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) 
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		initialize();
 	}
 	
 	
+	private void initialize() 
+	{
+		jsObject = new JsObject(this);
+		mMyHandlerImageDataResult = new MyHandlerImageDataResult();
+		jsObject.addImageDataResultHandler(mMyHandlerImageDataResult);
+		mImgCaptcha = (ImageView)findViewById(R.id.imgCaptcha);	
+	}
+
+
+	public class MyHandlerImageDataResult implements HandlerImageDataResult
+	{
+		@Override
+		public void onConvertComplete(final byte[] imageData) 
+		{
+			runOnUiThread(new Runnable() 
+			{
+				public void run() 
+				{
+				  ImageUtil.setImageViewWithByteArray(mImgCaptcha, imageData);
+				}
+			});
+		}
+	}
+		
 	public void btnGetCaptchaOnClick(View sender)
 	{
-		Toast.makeText(this,"I'am working !", Toast.LENGTH_LONG).show();
+	    jsObject.getWebView().loadUrl(URL_FORM);
 	}
 }
