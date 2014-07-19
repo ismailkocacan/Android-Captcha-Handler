9d11600a989ff2d170dcbce1b34521c883b9aec4
ismail <ismailkocacan@gmail.com>
2014-07-19 22:50:03 +0300

    modified

--- test/src/com/test/captchahandler/MainActivity.java
+++ test/src/com/test/captchahandler/MainActivity.java
@@ -4,12 +4,21 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	protected void onCreate(Bundle savedInstanceState) 
+	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+	}
+	
+	
+	public void btnGetCaptchaOnClick(View sender)
+	{
+		Toast.makeText(this,"I'am working !", Toast.LENGTH_LONG).show();
 	}
 }
