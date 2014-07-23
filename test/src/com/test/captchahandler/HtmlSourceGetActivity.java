a1c0e1e8e45326c6c840ce93e881ac8f61639ab2
ismail <ismailkocacan@gmail.com>
2014-07-24 02:09:38 +0300

    getting html code from webview content

---
+++ test/src/com/test/captchahandler/HtmlSourceGetActivity.java
@@ -0,0 +1,61 @@
+package com.test.captchahandler;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.widget.Toast;
+import com.stackdeveloper.lib.CustomWebChromeClient;
+import com.stackdeveloper.lib.CustomWebViewClient;
+import com.stackdeveloper.lib.HandlerPageLoad;
+import com.stackdeveloper.lib.HandlerWebViewHtmlContent;
+import com.stackdeveloper.lib.JsObject;
+
+public class HtmlSourceGetActivity extends Activity 
+{
+	private JsObject mJsObject;
+	private WebView mWebView1;
+	private MyWebViewPageLoadHandler mMyWebViewPageLoadHandler;
+	private MyHandlerWebViewHtmlContent mMyHandlerWebViewHtmlContent;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) 
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_html_source_get);
+		initialize();
+	}
+	
+	public class MyWebViewPageLoadHandler implements HandlerPageLoad
+	{
+		@Override
+		public void onPageLoad(WebView webView, String url) 
+		{		 
+			mJsObject.jsGetHtmlContent(mMyHandlerWebViewHtmlContent);
+		}	
+	}
+	
+	public class MyHandlerWebViewHtmlContent implements HandlerWebViewHtmlContent
+	{
+		@Override
+		public void onGetHtmlContent(String htmlContent) 
+		{
+		   Toast.makeText(getApplicationContext(), htmlContent, Toast.LENGTH_LONG).show();
+		}	
+	}
+	
+	private void initialize()
+	{
+		mWebView1 = (WebView)findViewById(R.id.webView1);
+		mJsObject = new JsObject(this, mWebView1);
+		
+		mMyWebViewPageLoadHandler = new MyWebViewPageLoadHandler();
+		mMyHandlerWebViewHtmlContent = new MyHandlerWebViewHtmlContent();
+		
+		CustomWebViewClient webViewClient = new CustomWebViewClient();
+		webViewClient.addHandlerPageLoad(mMyWebViewPageLoadHandler);
+		mJsObject.getWebView().setWebViewClient(webViewClient);
+		
+		CustomWebChromeClient chromeClient = new CustomWebChromeClient();
+		mJsObject.getWebView().setWebChromeClient(chromeClient);
+	}
+}
